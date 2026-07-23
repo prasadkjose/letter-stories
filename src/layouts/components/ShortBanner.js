337572906487a26d7050f07a4a84c44694c05050
prasad koshy jose <prasadkjose@gmail.com>
2026-07-23 12:47:33 -0600

    Fix: IMage size for short banner

--- src/layouts/components/ShortBanner.js
+++ src/layouts/components/ShortBanner.js
@@ -5,19 +5,21 @@ import Link from "next/link";
 const ShortBanner = ({ data }) => {
   const { icon, image, title, content, link } = data;
   return (
-    <div className="feature-card m-4 rounded-md border border-transparent px-7 py-16 shadow-[0px_4px_25px_rgba(0,0,0,.05)] transition-all duration-300  hover:border-[#ffece4] hover:shadow-none">
+    <div className="feature-card m-4 flex h-full flex-col rounded-md border border-transparent px-7 py-16 shadow-[0px_4px_25px_rgba(0,0,0,.05)] transition-all duration-300  hover:border-[#ffece4] hover:shadow-none">
       {icon && (
-        <div className="feature-card-icon inline-flex h-20 w-20 items-center justify-center rounded-md border border-[#fff7f3] text-primary">
+        <div className="feature-card-icon inline-flex h-20 w-20 self-center justify-center rounded-md border border-[#fff7f3] text-primary">
           <FeatherIcon icon={icon} />
         </div>
       )}
       {image && (
-        <ImageFallback
-          className=" relative object-contain "
-          src={image}
-          fill={true}
-          priority={true}
-        />
+        <div className="relative flex-1 min-h-[220px]">
+          <ImageFallback
+            className="object-cover"
+            src={image}
+            fill={true}
+            priority={true}
+          />
+        </div>
       )}
       <h3 className="h4 mb-5 mt-6">{title}</h3>
       <p>{content}</p>
